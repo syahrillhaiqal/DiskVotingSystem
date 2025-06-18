c8f317f7d2d4998924b034439906a4099658c2a4
syahrillhaiqal <syahrilhaiqal5@gmail.com>
2025-06-18 21:32:49 +0800

    first big update

---
+++ assets/js/tailwindConfig.js
@@ -0,0 +1,23 @@
+tailwind.config = {
+        theme: {
+          extend: {
+            colors: {
+              primary: {
+                50: '#f0f9f4',
+                100: '#dcf2e3',
+                200: '#bce5cc',
+                300: '#8dd1a8',
+                400: '#4a7c59',
+                500: '#2d5a27',
+                600: '#1a3d1a',
+                700: '#0f2a0f',
+                800: '#0a1f0a',
+                900: '#051405',
+              }
+            },
+            animation: {
+              'spin-slow': 'spin 1s linear infinite',
+            }
+          }
+        }
+      }
