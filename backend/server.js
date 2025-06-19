e5d5240fbba5d27ddbb778353734a1c135f9f5f4
syahrillhaiqal <syahrilhaiqal5@gmail.com>
2025-06-19 18:54:27 +0800

    bismillah4

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
