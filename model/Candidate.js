40688986149bd3f531ab41ba22605026e1ffd811
sykrwasd <umarsyakir16@gmail.com>
2025-06-20 20:17:06 +0800

    change student and candidates model

--- model/Candidate.js
+++ model/Candidate.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose')
 const Candidates = new mongoose.Schema({
     id : {type: String, required: true},
     name : {type: String, required: true},
+    candidate_pic : {type: String, required: true},
+    candidate_poster : {type: String, required: true},
+    candidates_vid : {type: String, required: true},
     total_votes : {type: Number, required: true},
 })
 
--- model/Students.js
+++ model/Students.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 const Students = new mongoose.Schema({
     id : {type: String, required: true},
+    name:  {type: String, required: true},
+    part: {type: String, required: true},
     voted : {type: Boolean, required: true},
 })
 
